dd65fdee5e7882df30a2e7e4e6e261ddcc4b79c8
Sergei Zavgorodnii <SZavgorodniy@alfabank.ru>
2019-12-22 18:29:28 +0300

    add mock server

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
-const CMD = '\x1b';
-
-const STYLE = {
-  CLEAR: CMD + '[0m',
-  GREEN: CMD + '[32m',
-  RED: CMD + '[31m'
+module.exports = {
+  test: require('./mbr-test.js'),
+  server: require('./server.js');
 };
-
-function test(cases) {
-  const caseTexts = Object.keys(cases);
-  let counter = 0;
-  let failed = 0;
-
-  function resolve() {
-    process.stdout.write(STYLE.GREEN + ' OK\n' + STYLE.CLEAR);
-    nextTest();
-  }
-
-  function fail(reason) {
-    process.stdout.write(STYLE.RED + ' FAIL\n' + reason + STYLE.CLEAR + '\n');
-    ++failed;
-    nextTest();
-  }
-
-  function nextTest() {
-    const currentCase = caseTexts[counter];
-
-    if (currentCase) {
-      ++counter;
-      process.stdout.write(currentCase);
-
-      cases[currentCase](resolve, fail);
-    } else if (failed) {
-      process.stdout.write(
-        '\nFailed ' + STYLE.RED + failed + STYLE.CLEAR + ' out of ' + STYLE.GREEN + counter + STYLE.CLEAR + '\n'
-      );
-      process.exit(1);
-    }
-  }
-  nextTest();
-}
-
-module.exports.test = test;
